b7ba6cc45e0e5c56d9ce9c711ae9032e5bc5f4d7
Brendan Byrd <GitHub@ResonatorSoft.org>
2020-03-28 13:40:25 -0400

    Prepare for v1.1.4

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("5e005136-e676-482b-95b7-c12a03d5e683")]
 
-[assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyVersion("1.1.4")]
+[assembly: AssemblyFileVersion("1.1.4")]
