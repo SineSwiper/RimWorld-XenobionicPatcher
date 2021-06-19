cc9c0ca186aaa06f9403726feec03e782c19d4f0
Brendan Byrd <GitHub@ResonatorSoft.org>
2021-06-19 14:16:22 -0400

    Add WorkerMod stat in ExtraSurgeryStats

--- Source/ExtraSurgeryStats.cs
+++ Source/ExtraSurgeryStats.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using HarmonyLib;
 using Verse;
 
 namespace XenobionicPatcher {
@@ -14,6 +15,9 @@ namespace XenobionicPatcher {
             category = DefDatabase<StatCategoryDef>.GetNamed("Surgery");
 
             yield return SurgeryCategoryStat(surgery);
+
+            StatDrawEntry workerModStat = WorkerModStat(surgery);
+            if (workerModStat != null) yield return workerModStat;
 
             yield return new StatDrawEntry(
                 category:    category,
@@ -75,6 +79,80 @@ namespace XenobionicPatcher {
                 reportText:  "Stat_Recipe_Surgery_SurgeryCategory_Desc".Translate(),
                 valueString: surgeryCategory,
                 displayPriorityWithinCategory: 4999
+            );
+        }
+
+        public static StatDrawEntry WorkerModStat (RecipeDef surgery) {
+            // Attempt to figure out which mod the workerClass belongs to
+            string moddedTitle          = null;
+
+            string         surgeryWorkerNamespace = surgery.workerClass.Namespace;
+            Assembly       surgeryWorkerAssembly  = surgery.workerClass.Assembly;
+            List<Assembly> surgeryDefAssemblies   = surgery.modContentPack?.assemblies?.loadedAssemblies;
+
+            if (surgeryDefAssemblies == null || surgeryWorkerAssembly == null) return null;
+
+            // Diving into assembly properties, especially ones that didn't load correctly, might run into exceptions...
+            try {
+                ModContentPack surgeryWorkerModPack = LoadedModManager.RunningModsListForReading.FirstOrFallback( mcp =>
+                    mcp.assemblies.GetType().Namespace == surgeryWorkerNamespace
+                );
+                moddedTitle = surgeryWorkerModPack != null ? surgeryWorkerModPack.Name : surgeryWorkerNamespace;
+            }
+            catch (Exception e) {
+                Base.Instance.ModLogger.Warning(
+                    "Could not compare surgeryDef assemblies (from {0}) with the current mod list: {1}",
+                    surgery.defName, e
+                );
+                return null;
+            };
+
+            // Only look for cases where surgery's workerClass is non-core
+            if (moddedTitle == null || moddedTitle == "RimWorld") return null;
+
+            string moddedTitleFormatted = "<i>" + moddedTitle + "</i>";
+
+            // Use "What's That Mod" color settings, if it exists
+            if (ModLister.GetActiveModWithIdentifier("co.uk.epicguru.whatsthatmod") != null) {
+                // Amazingly difficult to reflect against a namespace we don't even require in the assembly
+                Type WTM_ModSettings_Type = Helpers.SafeTypeByName("WhatsThatMod.WTM_ModSettings");
+                Type WTM_ModCore_Type     = Helpers.SafeTypeByName("WhatsThatMod.ModCore");
+
+                if (WTM_ModSettings_Type != null && WTM_ModCore_Type != null) {
+                    string template = null;
+
+                    // This isn't important enough to crash the UI
+                    try {
+                        // [Reflection] ModCore.Instance
+                        object WTM_Instance = Traverse.Create(WTM_ModCore_Type).Property("Instance").GetValue();
+
+                        // [Reflection] ModCore.Instance.GetSettings<WTM_ModSettings>() (generic method)
+                        object WTM_Settings = 
+                            AccessTools.Method(WTM_ModCore_Type, "GetSettings").
+                            MakeGenericMethod(new Type[] { WTM_ModSettings_Type }).
+                            Invoke(WTM_Instance, new object[] { })
+                        ;
+                    
+                        // [Reflection] template = ModCore.Instance.MakeTemplate(string)
+                        template = (string)AccessTools.Method(WTM_ModCore_Type, "MakeTemplate").Invoke(null, new object[] { WTM_Settings });
+                    }
+                    catch (Exception e) {
+                        Base.Instance.ModLogger.Warning(
+                            "Could not acquire WTM mod string template, while looking at {0}: {1}",
+                            surgery.defName, e
+                        );
+                    };
+
+                    if (template != null) moddedTitleFormatted = string.Format(template, moddedTitle).Replace("\n", "");
+                }
+            }
+
+            return new StatDrawEntry(
+                category:    category,
+                label:       "Stat_Recipe_Surgery_WorkerMod_Name".Translate(),
+                reportText:  "Stat_Recipe_Surgery_WorkerMod_Desc".Translate(),
+                valueString: moddedTitleFormatted,
+                displayPriorityWithinCategory: 4995
             );
         }
 
